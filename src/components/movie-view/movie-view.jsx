29059e801cfbe621f8a33c93574d8e25bda0307b
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2022-11-24 19:01:39 +0100

    Adding Bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col, Button } from "react-bootstrap";
+
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
@@ -7,22 +10,34 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-
-      </div>
+      <Row>
+        <Col>
+          <div className="movie-view">
+            <div className="movie-poster">
+              <img src={movie.ImagePath} />
+            </div>
+            <div className="movie-title">
+              <span className="label">Title: </span>
+              <span className="value">{movie.Title}</span>
+            </div>
+            <div className="movie-description">
+              <span className="label">Description: </span>
+              <span className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-director">
+              <span className="label">Director: </span>
+              <span className="value">{movie.Director.Name}</span>
+            </div>
+            <div className="movie-genre">
+              <span className="label">Genre: </span>
+              <span className="value">{movie.Genre.Name}</span>
+            </div>
+            <Button className="back-button mt-2"
+              variant="secondary"
+              onClick={() => { onBackClick(null); }}>Back</Button>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
